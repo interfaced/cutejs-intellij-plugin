6b36d457bc7741057af8af077ab24561f4989388
reeFridge <reefridgerator@gmail.com>
2018-05-11 02:40:22 +0700

    Implemented typed handler delegate for autoclose template blocks on type second brace

---
+++ src/main/kotlin/org/cutejs/ide/typing/CuteBlockTypedHandler.kt
@@ -0,0 +1,40 @@
+package org.cutejs.ide.typing
+
+import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Pair
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiFile
+
+import org.cutejs.lang.CuteLanguage
+import org.cutejs.lang.psi.CuteFile
+
+class CuteBlockTypedHandler : TypedHandlerDelegate() {
+    override fun beforeCharTyped(c: Char, project: Project, editor: Editor, file: PsiFile, fileType: FileType): Result {
+        if (file !is CuteFile) return Result.CONTINUE
+        val offset = editor.caretModel.offset
+
+        val delimiters = Pair.create("{", "}}")
+        val openBraceLength = (delimiters.first as String).length
+
+        if (offset < openBraceLength) {
+            return Result.CONTINUE
+        }
+
+        val previousChars = editor.document.getText(TextRange(offset - openBraceLength, offset))
+
+        if (previousChars == delimiters.first) {
+            val elementAt = file.viewProvider.findElementAt(offset, CuteLanguage.INSTANCE)
+
+            if (elementAt?.text?.contains("}}") != null) {
+                return Result.CONTINUE
+            }
+
+            editor.document.insertString(offset, delimiters.second)
+        }
+
+        return Result.CONTINUE
+    }
+}
