ce173bfa01a0f2cc6d16c770e255f19c0acd98d7
reeFridge <reefridgerator@gmail.com>
2018-05-11 22:00:09 +0700

    Fixed auto-close template brace

--- src/main/kotlin/org/cutejs/ide/typing/CuteBlockTypedHandler.kt
+++ src/main/kotlin/org/cutejs/ide/typing/CuteBlockTypedHandler.kt
@@ -2,20 +2,21 @@ package org.cutejs.ide.typing
 
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.StdFileTypes
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Pair
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
+import org.cutejs.lang.CuteFileType
 
 import org.cutejs.lang.CuteLanguage
 import org.cutejs.lang.parser.CuteParserDefinition
-import org.cutejs.lang.psi.CuteFile
 import org.cutejs.lang.psi.CuteTypes
 
 class CuteBlockTypedHandler : TypedHandlerDelegate() {
     override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile): Result {
-        if (file !is CuteFile) return Result.CONTINUE
+        if (file.fileType !in arrayOf(CuteFileType.INSTANCE, StdFileTypes.HTML)) return Result.CONTINUE
+
         val offset = editor.caretModel.offset
 
         val blockOpenSymbols = arrayOf(' ', '@', '$', '=', '-', '%', '*', '#')
@@ -44,7 +45,6 @@ class CuteBlockTypedHandler : TypedHandlerDelegate() {
         }
 
         val previousChars = editor.document.getText(TextRange(offset - openBraceLength, offset))
-
         if (previousChars == delimiters.first) {
             val elementAt = file.viewProvider.findElementAt(offset, CuteLanguage.INSTANCE)
 
