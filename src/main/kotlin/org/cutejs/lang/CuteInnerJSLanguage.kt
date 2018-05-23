ec667d84fb501dd6f8b2ab65c32a3add41d7724c
reeFridge <reefridgerator@gmail.com>
2018-05-23 16:24:40 +0700

    Use dialect option holder of JS

--- src/main/kotlin/org/cutejs/lang/CuteInnerJSLanguage.kt
+++ src/main/kotlin/org/cutejs/lang/CuteInnerJSLanguage.kt
@@ -3,7 +3,7 @@ package org.cutejs.lang
 import com.intellij.lang.javascript.DialectOptionHolder
 import com.intellij.lang.javascript.JSLanguageDialect
 
-class CuteInnerJSLanguage protected constructor() : JSLanguageDialect("CuteInnerJS", DialectOptionHolder.OTHER) {
+class CuteInnerJSLanguage protected constructor() : JSLanguageDialect("CuteInnerJS", DialectOptionHolder.JS_1_5) {
     override fun getFileExtension(): String {
         return "js"
     }
