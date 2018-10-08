26b582bf912cc03c7a3a4a2a737448c954a6c762
reeFridge <reefridgerator@gmail.com>
2018-10-09 02:06:32 +0700

    Move base implementation of visitElement method into CuteElementVisitor

--- src/main/kotlin/org/cutejs/lang/psi/visitor/CuteRecursiveElementVisitor.kt
+++ src/main/kotlin/org/cutejs/lang/psi/visitor/CuteRecursiveElementVisitor.kt
@@ -1,18 +1,10 @@
 package org.cutejs.lang.psi.visitor
 
 import com.intellij.psi.PsiElement
-import org.cutejs.lang.psi.CuteExportArgs
-import org.cutejs.lang.psi.CuteNamespace
-import org.cutejs.lang.psi.CuteTypedef
 
 open class CuteRecursiveElementVisitor : CuteElementVisitor() {
     override fun visitElement(element: PsiElement?) {
-        when (element) {
-            is CuteNamespace -> visitNamespace(element)
-            is CuteExportArgs -> visitExportArgs(element)
-            is CuteTypedef -> visitTypedef(element)
-        }
-
+        super.visitElement(element)
         element?.acceptChildren(this)
     }
 }
