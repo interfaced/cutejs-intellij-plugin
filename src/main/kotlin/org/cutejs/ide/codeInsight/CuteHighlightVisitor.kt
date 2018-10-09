387615e08506c2f23f201bb2c8165c8a2368173b
reeFridge <reefridgerator@gmail.com>
2018-10-09 15:09:48 +0700

    Dynamic highlighting for namespace and export identifiers

--- src/main/kotlin/org/cutejs/ide/codeInsight/CuteHighlightVisitor.kt
+++ src/main/kotlin/org/cutejs/ide/codeInsight/CuteHighlightVisitor.kt
@@ -5,49 +5,36 @@ import com.intellij.codeInsight.daemon.impl.analysis.HighlightInfoHolder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiWhiteSpace
-import org.cutejs.lang.psi.CuteExportArgs
-import org.cutejs.lang.psi.CuteFile
-import org.cutejs.lang.psi.CuteNamespace
-import org.cutejs.lang.psi.CuteTypedef
 import org.cutejs.lang.psi.visitor.CuteRecursiveElementVisitor
 import com.intellij.codeInsight.daemon.impl.HighlightInfoType
 import com.intellij.codeInsight.daemon.impl.HighlightInfo
 import org.cutejs.ide.highlight.CuteHighlighter
-
+import org.cutejs.lang.psi.*
 
 class CuteHighlightVisitor : CuteRecursiveElementVisitor(), HighlightVisitor {
     private var infoHolder: HighlightInfoHolder? = null
 
     override fun visitNamespace(element: CuteNamespace) {
-        val lastNamespaceIdentifier = element.namespaceArgs.namespaceIdentifier?.lastChild
+        val namespaceIdentifier = element.namespaceArgs.namespaceIdentifier ?: return
+        val cuteFile = element.containingFile as CuteFile
+        val generatedFile = cuteFile.getOrFindGeneratedFile()
 
-        if (lastNamespaceIdentifier != null) {
-            val builder = HighlightInfo.newHighlightInfo(HighlightInfoType.INFORMATION)
-            builder.textAttributes(CuteHighlighter.DECLARATION)
-            builder.range(lastNamespaceIdentifier)
-
-            infoHolder?.add(builder.create())
+        if (generatedFile != null) {
+            val lastIdentifier = namespaceIdentifier.lastChild
+            highlightDeclaration(lastIdentifier)
         }
     }
 
     override fun visitExportArgs(element: CuteExportArgs) {
-        val identifier = element.firstChild
-
-        val builder = HighlightInfo.newHighlightInfo(HighlightInfoType.INFORMATION)
-        builder.textAttributes(CuteHighlighter.PROPERTY)
-        builder.range(identifier)
-
-        infoHolder?.add(builder.create())
+        highlightPropertyIfResolved(element.firstChild)
     }
 
     override fun visitTypedef(element: CuteTypedef) {
-        val property = element.thisProperty.lastChild
+        highlightPropertyIfResolved(element.thisProperty.lastChild)
+    }
 
-        val builder = HighlightInfo.newHighlightInfo(HighlightInfoType.INFORMATION)
-        builder.textAttributes(CuteHighlighter.PROPERTY)
-        builder.range(property)
-
-        infoHolder?.add(builder.create())
+    override fun visit(element: PsiElement) {
+        element.accept(this)
     }
 
     override fun analyze(file: PsiFile, updateWholeFile: Boolean, holder: HighlightInfoHolder, action: Runnable): Boolean {
@@ -57,10 +44,6 @@ class CuteHighlightVisitor : CuteRecursiveElementVisitor(), HighlightVisitor {
         return true
     }
 
-    override fun visit(element: PsiElement) {
-        element.accept(this)
-    }
-
     override fun visitWhiteSpace(space: PsiWhiteSpace?) {}
 
     override fun clone(): HighlightVisitor = CuteHighlightVisitor()
@@ -68,4 +51,29 @@ class CuteHighlightVisitor : CuteRecursiveElementVisitor(), HighlightVisitor {
     override fun suitableForFile(file: PsiFile): Boolean = file is CuteFile
 
     override fun order(): Int = 1
+
+    private fun highlightPropertyIfResolved(element: PsiElement) {
+        val cuteFile = element.containingFile as CuteFile
+        val generatedFile = cuteFile.getOrFindGeneratedFile() ?: return
+
+        if (CuteResolveUtil.findElementsInFile(element.node.text, generatedFile) != null) {
+            highlightProperty(element)
+        }
+    }
+
+    private fun highlightProperty(element: PsiElement) {
+        val builder = HighlightInfo.newHighlightInfo(HighlightInfoType.INFORMATION)
+        builder.textAttributes(CuteHighlighter.PROPERTY)
+        builder.range(element)
+
+        infoHolder?.add(builder.create())
+    }
+
+    private fun highlightDeclaration(element: PsiElement) {
+        val builder = HighlightInfo.newHighlightInfo(HighlightInfoType.INFORMATION)
+        builder.textAttributes(CuteHighlighter.DECLARATION)
+        builder.range(element)
+
+        infoHolder?.add(builder.create())
+    }
 }
