706a51adbc60dbc524b9ee4a247a90680abbc439
reeFridge <reefridgerator@gmail.com>
2018-10-09 15:14:52 +0700

    Refactor CuteGotoDeclaration using CuteResolveUtil and CuteFile abilities

--- src/main/kotlin/org/cutejs/ide/codeInsight/CuteGotoDeclarationHandler.kt
+++ src/main/kotlin/org/cutejs/ide/codeInsight/CuteGotoDeclarationHandler.kt
@@ -1,19 +1,14 @@
 package org.cutejs.ide.codeInsight
 
 import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandler
-import com.intellij.lang.javascript.psi.JSPsiElementBase
-import com.intellij.lang.javascript.psi.resolve.JSClassResolver.findElementsByNameIncludingImplicit
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.search.ProjectScope
 import com.intellij.psi.util.PsiTreeUtil.getParentOfType
 import org.cutejs.lang.psi.CuteFile
+import org.cutejs.lang.psi.CuteResolveUtil
 import org.cutejs.lang.psi.CuteTypes.T_IDENTIFIER
 import org.cutejs.lang.psi.impl.*
-
-
 
 class CuteGotoDeclarationHandler : GotoDeclarationHandler {
     override fun getActionText(p0: DataContext?): String? {
@@ -29,34 +24,14 @@ class CuteGotoDeclarationHandler : GotoDeclarationHandler {
 
         if (!(isChildOfNamespace || isChildOfExport || isChildOfTypedef) || !isIdentifier) return null
 
-        val namespaceIdentifier = getParentOfType(element, CuteNamespaceIdentifierImpl::class.java)
-        val project = editor?.project ?: return null
-        val projectScope = ProjectScope.getProjectScope(project)
         val cuteFile = element?.containingFile as CuteFile
-        val templateNamespace = namespaceIdentifier?.text ?: cuteFile.templateNamespace() ?: return null
-        val templateIdentifier = templateNamespace.split(".").last()
-
-        fun findByNameInNamespace(namespace: String, name: String, scope: GlobalSearchScope): Collection<JSPsiElementBase>  {
-            return findElementsByNameIncludingImplicit(name, scope, false).filter {
-                val fileName = it.containingFile.virtualFile.name
-                val isJstGenFile = fileName.endsWith(".jst.js")
-                val elementNamespace = it.namespace?.qualifiedName
-                val isTargetNamespace = elementNamespace != null && namespace == "$elementNamespace.$name"
-
-                isJstGenFile && isTargetNamespace
-            }
-        }
-
-        val nsElements = findByNameInNamespace(templateNamespace, templateIdentifier, projectScope)
+        val cuteGeneratedFile = cuteFile.getOrFindGeneratedFile() ?: return null
 
         if (isChildOfNamespace && isLeaf) {
-            return nsElements.toTypedArray()
+            return arrayOf(cuteGeneratedFile.firstChild)
         } else if (isChildOfExport || isChildOfTypedef) {
             val identifier = element.node?.text ?: return null
-            val files = nsElements.map { it.containingFile.virtualFile }
-            val scope = GlobalSearchScope.filesScope(project, files)
-
-            return findElementsByNameIncludingImplicit(identifier, scope, false).toTypedArray()
+            return CuteResolveUtil.findElementsInFile(identifier, cuteGeneratedFile)
         }
 
         return null
