4f2ab42498e3fcb3c17a6b1a3e43522ad3fe4a8a
reeFridge <reefridgerator@gmail.com>
2018-11-08 23:39:12 +0700

    Patching JS tokens contents before parsing

--- src/main/kotlin/org/cutejs/file/CuteJSElementType.kt
+++ src/main/kotlin/org/cutejs/file/CuteJSElementType.kt
@@ -2,11 +2,57 @@ package org.cutejs.file
 
 import com.intellij.psi.templateLanguages.TemplateDataElementType
 import com.intellij.lang.Language
+import com.intellij.lexer.Lexer
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.templateLanguages.TemplateLanguageFileViewProvider
 
-class CuteJSElementType(debugName: String, language: Language, templateElementType: IElementType, outerElementType: IElementType) : TemplateDataElementType(debugName, language, templateElementType, outerElementType) {
+class CuteJSElementType(
+        debugName: String,
+        language: Language,
+        private val templateElementType: IElementType,
+        private val expressionElementType: IElementType,
+        outerElementType: IElementType
+) : TemplateDataElementType(debugName, language, templateElementType, outerElementType) {
     override fun getTemplateFileLanguage(viewProvider: TemplateLanguageFileViewProvider): Language {
         return language
+    }
+
+    override fun createTemplateText(sourceCode: CharSequence, baseLexer: Lexer, rangeCollector: RangeCollector): CharSequence {
+        val result = StringBuilder(sourceCode.length)
+        baseLexer.start(sourceCode)
+
+        // idk, why ES6 parser require wrapping code into block-statement, but is necessary for arrow-functions
+        result.append("{")
+        rangeCollector.addRangeToRemove(TextRange(0, 1))
+
+        var currentRange = TextRange.EMPTY_RANGE
+        while (baseLexer.tokenType != null) {
+            val newRange = TextRange.create(baseLexer.tokenStart, baseLexer.tokenEnd)
+
+            assert(currentRange.endOffset == newRange.startOffset) {
+                "Inconsistent tokens stream from $baseLexer: $currentRange followed by $newRange"
+            }
+
+            currentRange = newRange
+            when (baseLexer.tokenType) {
+                templateElementType -> appendCurrentTemplateToken(result, sourceCode, baseLexer, rangeCollector)
+                expressionElementType -> appendCurrentExpressionToken(result, baseLexer, rangeCollector)
+                else -> rangeCollector.addOuterRange(newRange)
+            }
+            baseLexer.advance()
+        }
+
+        result.append("}")
+        rangeCollector.addRangeToRemove(TextRange(currentRange.endOffset, currentRange.endOffset + 1))
+        return result
+    }
+
+    private fun appendCurrentExpressionToken(result: java.lang.StringBuilder, lexer: Lexer, collector: RangeCollector) {
+        val wrappedExpr = "(${lexer.tokenText});"
+        collector.addRangeToRemove(TextRange(lexer.tokenStart, lexer.tokenStart + 1))
+        collector.addRangeToRemove(TextRange(lexer.tokenEnd, lexer.tokenEnd + 2))
+
+        result.append(wrappedExpr)
     }
 }
