57f60916cb7042a1856491cf747f5a90113cc475
reeFridge <reefridgerator@gmail.com>
2018-11-08 23:37:02 +0700

    HTML as data language

--- src/main/kotlin/org/cutejs/file/CuteFileViewProvider.kt
+++ src/main/kotlin/org/cutejs/file/CuteFileViewProvider.kt
@@ -2,55 +2,35 @@ package org.cutejs.file
 
 import com.intellij.lang.Language
 import com.intellij.lang.LanguageParserDefinitions
-import com.intellij.openapi.fileTypes.PlainTextLanguage
-import com.intellij.openapi.fileTypes.StdFileTypes
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.LanguageSubstitutors
 import com.intellij.psi.MultiplePsiFilesPerDocumentFileViewProvider
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.impl.source.PsiFileImpl
 import com.intellij.psi.templateLanguages.TemplateDataElementType
-import com.intellij.psi.templateLanguages.TemplateDataLanguageMappings
-import com.intellij.psi.templateLanguages.TemplateLanguage
 import com.intellij.psi.templateLanguages.TemplateLanguageFileViewProvider
 import org.cutejs.lang.CuteLanguage
 import org.cutejs.lang.psi.CuteTypes
 import org.cutejs.lang.CuteJSLanguage
 
-class CuteFileViewProvider : MultiplePsiFilesPerDocumentFileViewProvider, TemplateLanguageFileViewProvider {
-    private val myTemplateDataLanguage: Language
-
-    constructor(manager: PsiManager, file: VirtualFile, physical: Boolean) : super(manager, file, physical) {
-        var dataLang = TemplateDataLanguageMappings.getInstance(manager.project).getMapping(file)
-        if (dataLang == null) dataLang = StdFileTypes.HTML.language
-
-        myTemplateDataLanguage = if (dataLang is TemplateLanguage) {
-            PlainTextLanguage.INSTANCE
-        } else {
-            LanguageSubstitutors.INSTANCE.substituteLanguage(dataLang, file, manager.project)
-        }
-    }
-
-    constructor(psiManager: PsiManager, virtualFile: VirtualFile, physical: Boolean, myTemplateDataLanguage: Language) : super(psiManager, virtualFile, physical) {
-        this.myTemplateDataLanguage = myTemplateDataLanguage
-    }
-
+class CuteFileViewProvider(psiManager: PsiManager, virtualFile: VirtualFile, physical: Boolean)
+    : MultiplePsiFilesPerDocumentFileViewProvider(psiManager, virtualFile, physical), TemplateLanguageFileViewProvider {
+    private val templateDataLanguage: Language = CuteLanguage.getDefaultTemplateLang()
 
     override fun getBaseLanguage(): Language {
         return CuteLanguage.INSTANCE
     }
 
     override fun getTemplateDataLanguage(): Language {
-        return myTemplateDataLanguage
+        return templateDataLanguage
     }
 
     override fun getLanguages(): Set<Language> {
-        return setOf(CuteLanguage.INSTANCE, CuteJSLanguage.INSTANCE, myTemplateDataLanguage)
+        return setOf(CuteLanguage.INSTANCE, CuteJSLanguage.INSTANCE, templateDataLanguage)
     }
 
     override fun cloneInner(file: VirtualFile): MultiplePsiFilesPerDocumentFileViewProvider {
-        return CuteFileViewProvider(manager, file, false, myTemplateDataLanguage)
+        return CuteFileViewProvider(manager, file, false)
     }
 
     override fun createFile(lang: Language): PsiFile? {
@@ -60,7 +40,7 @@ class CuteFileViewProvider : MultiplePsiFilesPerDocumentFileViewProvider, Templa
             CuteLanguage.INSTANCE -> {
                 return parserDefinition.createFile(this)
             }
-            myTemplateDataLanguage -> {
+            templateDataLanguage -> {
                 val file = parserDefinition.createFile(this) as PsiFileImpl
                 file.contentElementType = TEMPLATE_MARKUP_DATA_TYPE
 
@@ -80,6 +60,6 @@ class CuteFileViewProvider : MultiplePsiFilesPerDocumentFileViewProvider, Templa
         private val TEMPLATE_MARKUP_DATA_TYPE = TemplateDataElementType("TEMPLATE_MARKUP", CuteLanguage.INSTANCE,
                 CuteTypes.T_DATA, CuteTypes.T_OUTER_DATA)
         private val TEMPLATE_JS_DATA_TYPE = CuteJSElementType("TEMPLATE_JS", CuteJSLanguage.INSTANCE,
-                CuteTypes.T_EVAL_CHAR, CuteTypes.T_OUTER_DATA)
+                CuteTypes.T_EVAL, CuteTypes.T_EVAL_EXPRESSION, CuteTypes.T_OUTER_DATA)
     }
 }
